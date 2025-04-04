45aacd2a85017a69377596b9c05c0c0bb2f021d2
AndreSSS1022 <casuareza@academia.usbbog.edu.co>
2025-04-04 01:32:30 +0000

    commit

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -11,3 +11,4 @@ MERGE INTO FEID_ENTITY (id, nombre_cancion, duracion, artista)
         ('3c7e24cf-05a1-4393-8ad4-5063f078eb2e', 'UN COPO DE CIELO', '3:00', 'Feid'),
         ('2a8b3df9-356f-4235-b630-7d87f5c10f4c', 'DANDY', '3:19', 'Feid'),
         ('77f1c4cd-2b97-49a1-8f46-4c4a6d86a6e3', 'BELLO', '3:30', 'Feid');
+}
